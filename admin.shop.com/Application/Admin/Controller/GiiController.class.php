0cac0e60440d6d67cb0eb576ec358db460d056d2
zhouliwen <903493655@qq.com>
2016-01-10 23:01:41 +0800

    Gii的控制器和模型

---
+++ admin.shop.com/Application/Admin/Controller/GiiController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zlw
+ * Date: 2016/1/10
+ * Time: 23:00
+ */
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class GiiController extends Controller
+{
+
+}
---
+++ admin.shop.com/Application/Admin/Model/GiiModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zlw
+ * Date: 2016/1/10
+ * Time: 23:00
+ */
+
+namespace Admin\Model;
+
+
+use Think\Model;
+
+class GiiModel extends Model
+{
+
+}
